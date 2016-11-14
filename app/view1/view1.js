cb824b97fcb539bb6f861ca17f662c6f1733a32b
masih <masih@oven-fresh.com>
2016-11-15 04:57:27 +0530

    two component,s router on navigation

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,26 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('View1Ctrl', function($scope, ergastAPIservice) {
+    $scope.nameFilter = null;
+    $scope.driversList = [];
 
-}]);
+    ergastAPIservice.getDrivers().success(function (response) {
+        //Dig into the responde to get the relevant data
+        $scope.driversList = response.MRData.StandingsTable.StandingsLists[0].DriverStandings;
+    });
+})
+
+.factory('ergastAPIservice', function($http) {
+
+    var ergastAPI = {};
+
+    ergastAPI.getDrivers = function() {
+      return $http({
+        method: 'JSONP', 
+        url: 'http://ergast.com/api/f1/2013/driverStandings.json?callback=JSON_CALLBACK'
+      });
+    }
+
+    return ergastAPI;
+  });
