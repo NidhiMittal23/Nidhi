355f3bdcc0aef6044b376f80d10a058813c970f7
masih <masih@oven-fresh.com>
2016-12-28 12:39:43 +0530

    working on company setup (site setup)

--- app/DocsApp/company/company.js
+++ app/DocsApp/company/company.js
@@ -1,12 +1,21 @@
 var companyApp = angular.module('company', ['ui.router', 'company.controllers', 'company.services'])
 
 companyApp.config(function($stateProvider, $urlRouterProvider){
-	$stateProvider
-		.state('company',{
-			url: '/company',
-			parent: 'home',
-			templateUrl: 'DocsApp/company/templates/company-list.html',
+    $stateProvider
+        .state('company',{
+            url: '/company',
+            parent: 'home',
+            templateUrl: 'DocsApp/company/templates/company-list.html',
             controller: 'companyCtrl',
             authenticate: true
-		})
+        })
+
+        .state('addCompany', {
+            url: '/company/add',
+            parent: 'home',
+            templateUrl: 'DocsApp/company/templates/company-alter.html',
+            controller: 'companyAlterCtrl',
+            params: {},
+            authenticate: true
+        })
 })
--- app/DocsApp/document/document.js
+++ app/DocsApp/document/document.js
@@ -1,21 +1,21 @@
 var documentApp = angular.module('document', ['ui.router', 'document.controllers', 'document.services'])
 
 documentApp.config(function($stateProvider, $urlRouterProvider){
-	$stateProvider
-		.state('document', {
-			url: '/document',
-			parent: 'home',
-			templateUrl: 'DocsApp/document/templates/document-list.html',
-			controller: 'documentCtrl',
-			authenticate: true
-		})
+    $stateProvider
+        .state('document', {
+            url: '/document',
+            parent: 'home',
+            templateUrl: 'DocsApp/document/templates/document-list.html',
+            controller: 'documentCtrl',
+            authenticate: true
+        })
 
-		.state('docVersion', {
-			url: '/{name}',
-			parent: 'home',
-			templateUrl: 'DocsApp/document/templates/docVersion-list.html',
-			controller: 'docVersionCtrl',
-			params: {id: null, name: null},
-			authenticate: true
-		})
+        .state('docVersion', {
+            url: '/{name}',
+            parent: 'home',
+            templateUrl: 'DocsApp/document/templates/docVersion-list.html',
+            controller: 'docVersionCtrl',
+            params: {id: null, name: null},
+            authenticate: true
+        })
 })
