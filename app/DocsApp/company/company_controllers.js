23fc33f03d70dfb6a760f32b8ece487e5f918aa6
masih <masih@oven-fresh.com>
2016-12-28 12:39:43 +0530

    working on company setup (site setup)

--- app/DocsApp/company/company_controllers.js
+++ app/DocsApp/company/company_controllers.js
@@ -26,3 +26,97 @@ companyController.controller('companyCtrl', function($state, $scope, companyAPIs
         }
     })
 });
+
+companyController.controller('companyAlterCtrl', function($scope, $state, $stateParams, $q, companyAPIservice,
+    licenseAPIservice, verticalAPIservice) {
+    $scope.companyModel = {};
+    $scope.companySiteModel = {};
+    
+    if ($state.current.name == 'editCompany') {
+        $scope.isEdit = true;
+        var id = $stateParams.id
+        
+        // request to server to get detail of perticular company.
+        companyAPIservice.getCompanyDetails(id).success(function (response, status) {
+            //populate the input field with data.
+            $scope.companyModel = response;
+        })
+
+    }
+    else if ($state.current.name == 'addCompany') {
+        $scope.isEdit = false;
+    }
+
+    $scope.init = function () {
+        $scope.companySiteModel.selectedLicenseId = function(licenseId) {
+            console.log(licenseId);
+            $scope.companySiteModel.licenseSelected = licenseId;
+        }
+
+        $scope.companySiteModel.selectedVerticalId = function(verticalId) {
+            console.log(verticalId);
+            $scope.companySiteModel.verticalSelected = verticalId;
+        }
+
+        var licenses = licenseAPIservice.getlicense();
+        var verticals = verticalAPIservice.getVertical();
+
+        $q.all([verticals, licenses]).then(function(values) {
+            $scope.companySiteModel.verticalOption = values[0].data.results;
+            $scope.companySiteModel.licenseOption = values[1].data.results;
+        });
+    }
+
+    // on submit button click
+    $scope.addNewCompany = function() {
+        var params = $scope.companyModel;
+        // default industry has been to set to {'1': 'Food'}
+        params.industry = "1";
+        companyAPIservice.postCompanyDetail(params).success(function (response, status) {
+            console.log(response);
+            var companyName = params.name;
+            // Notification.success(companyName+' added successfully');
+        }).error( function(response, status) {
+            if (status == 400) {
+                if ('name' in response) {
+                    Notification.error(response['name'][0]);
+                }
+            }
+            else if (status == 500) {
+                Notification.error("Server error occured, Contact Admin");
+            }
+            else {
+                Notification.error("Error occured, Contact Admin");
+            }
+        })
+    }
+
+    // todo: complete POST.. adding site info
+    $scope.addNewCompanySite = function() {
+        var params = $scope.companySiteModel;
+        params.license = $scope.companySiteModel.licenseSelected;
+        delete params.licenseSelected;
+        params.vertical = $scope.companySiteModel.verticalSelected;
+        delete params.verticalSelected;
+        delete params.licenseOption;
+        delete params.verticalOption;
+        console.log(params);
+        // companyAPIservice.postCompanyDetail(params).success(function (response, status) {
+        //     console.log(response);
+        //     var companyName = params.name;
+        //     Notification.success(companyName+' added successfully');
+        // }).error( function(response, status) {
+        //     if (status == 400) {
+        //         if ('name' in response) {
+        //             Notification.error(response['name'][0]);
+        //         }
+        //     }
+        //     else if (status == 500) {
+        //         Notification.error("Server error occured, Contact Admin");
+        //     }
+        //     else {
+        //         Notification.error("Error occured, Contact Admin");
+        //     }
+        // })
+    }
+});
--- app/DocsApp/company/company_services.js
+++ app/DocsApp/company/company_services.js
@@ -5,13 +5,78 @@ companyService.factory('companyAPIservice', function($http) {
 	var companyAPI = {};
 
     var companyUrl = {
-        'endpoint': 'http://localhost:9000/company/'
+        'endpoint': 'http://localhost:9000/company/',
+        'siteEndpoint': 'http://localhost:9000/site/'
     }
 
     companyAPI.getCompany =function() {
         return $http({
             method: 'GET',
             url: companyUrl.endpoint
+        });
+    }
+
+    companyAPI.getCompanyDetails = function(id) {
+        return $http({
+            method: 'GET', 
+            url: companyUrl.endpoint + id + '/'
+        });
+    }
+
+    companyAPI.postCompanyDetail = function(params) {
+        var payload = new FormData();
+
+        payload.append('name', params.name);
+        payload.append('industry', params.industry);
+
+        return $http({
+            url: companyUrl.endpoint,
+            method: 'POST',
+            data: payload,
+            //assign content-type as undefined, the browser
+            //will assign the correct boundary for us
+            headers: { 'Content-Type': undefined},
+            //prevents serializing payload.  don't do it.
+            transformRequest: angular.identity
+        });
+    }
+
+    companyAPI.postCompanyDetail = function(params) {
+        var payload = new FormData();
+
+        payload.append('name', params.name);
+        payload.append('industry', params.industry);
+
+        return $http({
+            url: companyUrl.endpoint,
+            method: 'POST',
+            data: payload,
+            //assign content-type as undefined, the browser
+            //will assign the correct boundary for us
+            headers: { 'Content-Type': undefined},
+            //prevents serializing payload.  don't do it.
+            transformRequest: angular.identity
+        });
+    }
+
+    companyAPI.postCompanySiteDetail = function(params) {
+        var payload = new FormData();
+
+        payload.append('name', params.name);
+        payload.append('location', params.location);
+        // loop within params.vertical and params.license to complete payload
+        // form.append("vertical", "1");
+        // form.append("vertical", "2");
+
+        return $http({
+            url: companyUrl.endpoint,
+            method: 'POST',
+            data: payload,
+            //assign content-type as undefined, the browser
+            //will assign the correct boundary for us
+            headers: { 'Content-Type': undefined},
+            //prevents serializing payload.  don't do it.
+            transformRequest: angular.identity
         });
     }
 
