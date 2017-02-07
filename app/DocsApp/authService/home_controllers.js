186ade142203d75140c1122f909c85827b5ca412
masih <masih@oven-fresh.com>
2017-02-07 17:44:09 +0530

    working with home page html and css; TODO list

--- app/DocsApp/authService/home_controllers.js
+++ app/DocsApp/authService/home_controllers.js
@@ -13,11 +13,23 @@ homeController.controller('HomeController', function($http, $auth, $scope, $stat
     }
 
     $scope.navList = [
-    	{ name : 'License', val : 'license'},
-    	{ name : 'Category', val : 'category'},
-    	{ name : 'Industry', val : 'industry'},
-    	{ name : 'Vertical', val : 'vertical'},
-    	{ name : 'Company Management', val : 'company'},
-    	{ name : 'Document Management', val : 'document'}
+    	{ name : 'License', val : 'license', type: 'link'},
+    	{ name : 'Category', val : 'category', type: 'link'},
+    	{ name : 'Industry', val : 'industry', type: 'link'},
+    	{ name : 'Vertical', val : 'vertical', type: 'link'},
+    	{ name : 'Company Management', val : 'company', type: 'toggle'},
+    	{ name : 'Document Management', val : 'document', type: 'toggle'}
     ];
 });
+
+// todo navigation with respect to user
+// Make navigation such that docuemnt maangement->Manual, Checklist, Sop
+// Company Management->Company List
+// https://www.npmjs.com/package/angular-material-sidemenu sidemenu
+// try to figure navigation w.r.t user login.
+// 
+// Navigation: (GET /get_site_documents)
+// ON Portal(user is admin): 1.Docment Management(Fetch FoodProDocsSite documents) 2.Company every table view
+// (user is company employee): 1.Document Management(Fetch Perticular site documents) and others
+// 
+// Use same Document view to navigate through company documents
