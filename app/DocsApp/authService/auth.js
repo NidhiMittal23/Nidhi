735f2d927eff9256474b3f267e84449c2cdcf696
masih <masih@oven-fresh.com>
2017-02-17 14:13:51 +0530

    changed TOKEN name to JWT

--- app/DocsApp/authService/auth.js
+++ app/DocsApp/authService/auth.js
@@ -3,6 +3,7 @@ var authServiceApp = angular.module('authService', ['ui.router', 'authService.au
 
 authServiceApp.config(function($stateProvider, $urlRouterProvider, $authProvider) {
     // put urls at some global place
+    $authProvider.tokenType = 'JWT';
     $authProvider.loginUrl = 'http://localhost:9000/auth/login/';
     $authProvider.signupUrl = 'http://localhost:9000/auth/register/'    
 
