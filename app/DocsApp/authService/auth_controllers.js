b50f157952fb89ef7ea2a32f093f2358d5aec32c
masih <masih@oven-fresh.com>
2017-02-17 14:42:38 +0530

    installed angular-bootstrap and changed auth controller to have email as login credential

--- app/DocsApp/authService/auth_controllers.js
+++ app/DocsApp/authService/auth_controllers.js
@@ -35,7 +35,7 @@ authController.controller('AuthController', function($auth, $state) {
 
     vm.login = function() {
         var credentials = {
-            username: vm.username,
+            email: vm.email,
             password: vm.password
         }
         
